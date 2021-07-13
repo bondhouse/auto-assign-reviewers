4bf37e77ef5a6bce2cd5942936fa1d1e64894fe2
Dov Salomon <dov@yieldx.app>
2021-07-13 15:45:58 -0400

    check user activity in last 3 weeks

--- auto-assign-reviewer.js
+++ auto-assign-reviewer.js
@@ -15,25 +15,40 @@ module.exports = async ({ github, context, core }) => {
 
   // get open PRs on this repo
   // + get list of contributors to this repo
-  const [pullsResp, contributorsResp, collaboratorsResp] = await Promise.all([
-    github.pulls.list({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      per_page: 100,
-    }),
-    github.repos.listContributors({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      per_page: 100,
-    }),
-    github.repos.listCollaborators({
-      owner: context.repo.owner,
-      repo: context.repo.repo,
-      per_page: 100,
-    }),
-  ])
+  const [pullsResp, contributorsResp, collaboratorsResp, contributorStatsResp] =
+    await Promise.all([
+      github.pulls.list({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        per_page: 100,
+      }),
+      github.repos.listContributors({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        per_page: 100,
+      }),
+      github.repos.listCollaborators({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        per_page: 100,
+      }),
+      github.repos.getContributorsStats({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+      }),
+    ])
   const pulls = pullsResp.data
   const open_prs = pulls.length
+
+  const collaboratorStats = contributorStatsResp.data.reduce(
+    (res, s) => (
+      (res[s.author.login] = s.weeks
+        .slice(-3)
+        .reduce((acc, o) => acc + o.c + o.a + o.d, 0)),
+      res
+    ),
+    {}
+  )
 
   // filter out [bot] users
   const contributors = contributorsResp.data
@@ -61,22 +76,30 @@ module.exports = async ({ github, context, core }) => {
   })
 
   // remove the PR owner from contention
-  if (actor in reviews_per_reviewer) delete reviews_per_reviewer[actor]
-
-  console.log("before filter: reviews per reviewer:", reviews_per_reviewer)
+  if (actor in reviews_per_reviewer) {
+    console.log(`cannot assign ${actor} (PR owner) as reviewer`)
+    delete reviews_per_reviewer[actor]
+  }
 
   // filter out contributors that are not collaborators
   const collaborators = collaboratorsResp.data.map((c) => c.login)
   core.startGroup("collaborators")
   console.log(collaborators)
   core.endGroup()
-  for (user in reviews_per_reviewer)
-    if (!collaborators.includes(user)) delete reviews_per_reviewer[user]
-
-  console.log("reviews per reviewer:", reviews_per_reviewer)
+  for (user in reviews_per_reviewer) {
+    activity = collaboratorStats[user]
+    if (!activity)
+      console.log(`cannot assign ${user} (no activity) as reviewer`)
+    if (!collaborators.includes(user))
+      console.log(`cannot assign ${user} (not a collaborator) as reviewer`)
+    if (!activity || !collaborators.includes(user)) {
+      delete reviews_per_reviewer[user]
+    }
+  }
 
   const min_reviewer = min_key(reviews_per_reviewer)
 
+  console.log("reviews per reviewer:", reviews_per_reviewer)
   console.log("assigning review to", min_reviewer)
 
   core.setOutput("reviewer", min_reviewer)
